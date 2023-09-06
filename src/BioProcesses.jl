0633dfca6465f9f293c3349f183495b35fc01618
Fabian Müller <fabianmueller100295@gmail.com>
2023-09-06 15:48:20 +0200

    first lines

--- src/BioProcesses.jl
+++ src/BioProcesses.jl
@@ -1,5 +1,36 @@
 module BioProcesses
 
+using DataFrames
 # Write your package code here.
+
+mutable struct BioProcess 
+    name::String
+    data::DataFrame #complete with lot of NaN's for undefined times 
+
+    function BioProcess()
+        new("Test",DataFrame(a=[1,2]))
+    end
+end
+
+# Outer constructor
+function BioProcess()
+end
+
+bp = BioProcess()
+
+# all the functions, models that can be used for bioprocesses
+function calculator_function(var1,var2,var3)
+    return var1+var2+var3, var3-var2
+end
+
+function calc!(bioprocess::BioProcess, calcfun::Function;
+    interp=:highest, )
+end
+
+calc!(bp, calculator_function)
+
+
+c.data
+
 
 end
